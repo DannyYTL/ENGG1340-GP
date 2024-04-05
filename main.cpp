306d131b241f28cd3dccce530cdaf0cdab015e92
Wind.D <125264206+DannyYTL@users.noreply.github.com>
2024-04-05 14:23:02 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,16 @@
 
 using namespace std;
 
+char initialise_board() {
+    int x,y;
+    cout << "The width of the board: ";
+    cin >> x;
+    cout << "The height of the board: ";
+    cin >> y;
+    char board[x][y];
+    
+}
+
 int main() {
     
     return 0;
