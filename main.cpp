22191635a6eecdc436e6ef2324f7d0680356438c
Wind.D <125264206+DannyYTL@users.noreply.github.com>
2024-04-20 23:33:49 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -221,118 +221,8 @@ int ** read_file(string filename,int &x,int &y) {
         return board;
 }
 
-int main() {
-// customer input (separate function??) //
-    string options;
-    string xyl;
-    int x=1, y=1, level=0;  
-    cout << "what you want to do?\n New Game: N\n Load Game: L\n";
-    cin >> options;  
-    if (options == "N") {
-        while( y<3 || y>300 ){
-            cout << "The width of the board (3-300): ";
-            cin>>xyl;
-            istringstream iss(xyl);
-            iss >> y;
-        }
-        while( x<3 || x>300 ){
-            cout << "The height of the board (3-300): ";
-            cin>>xyl;
-            istringstream iss(xyl);
-            iss >> x;
-        }
-        while ( level<1 || level>10 ){
-            cout << "Choose a level (1-10):";
-            cin>>xyl;
-            istringstream iss(xyl);
-            iss >> level;
-        }
-        int num_of_mine = 0.1*level*x*y/5; //set 地雷 value
-        if (num_of_mine<1){ //set minimum 地雷 value
-            num_of_mine=1;
-        }
-    // //
-    // initialise_board //
-        int **initial_board = new int*[x];
-        for ( int i=0; i<x; ++i){
-            initial_board[i] = new int[y];
-        }
-
-        initialise_board(initial_board, x, y, num_of_mine);
-    //  //
-        display(initial_board, x, y, 0, 0);
-
-        //test input
-        string temp;
-        char mode;
-        int input_x=999, input_y=999;
-        bool die = 0, valid=0;
-        //cout<<"max_x: "<<y<<", max_y: "<<x<<endl;
-        cout<<"enter the mode and the coordinates!" << endl;
-        cout << "modes: 'e': excavate, 'f': flag, 'q': quit (e.g.: e x-axis y-axis)\n";
-        while(getline(cin,temp)){
-            istringstream iss(temp);
-            iss >> mode >> input_x >> input_y;
-            if (mode != 'q' && mode != 'e' && mode != 'f'){
-                cout<<"Please enter a valid mode"<<endl;
-                input_x=999;
-                input_y=999;
-                continue;
-            }
-            else if (mode != 'q' && (input_x < 0 || input_x >= y || input_y < 0 || input_y >= x)){
-                cout<<"Please enter a valid coordinates"<<endl;
-                input_x=999;
-                input_y=999;
-                continue;
-            }
-            if(mode=='q'){
-                save_to_file("Record.txt",initial_board,x,y);
-                cout << "Bye\n";
-                break;
-            }
-            
-            die = processing(initial_board, y, x, mode, input_x, input_y);
-            if(die !=1 && win(initial_board,x,y)==1){
-                display(initial_board, x, y, die, win);
-                cout << "you win\n";
-                break;
-            }
-            display(initial_board, x, y, die, 0);
-            if(die){                                                                                                                                                                                       
-                   cout <<  "█     █       █    █         ██████            █      █        █     █                                      █      █    █     █  █                   █            \n"        
-                            "████████     ███████      █████████████        █████████      ████████            ████████████████         █████████    ███████████████     ████████████████      \n"                      
-                            " ████         ███      ████        █████        ████            ███               ████       ███████         ████        ███        ██       ████      ███████    \n"                     
-                            "  ████       ██      ████            ████       ████            ███                ███           ████      █ ████        ███         █       ███           ████   \n"                  
-                            "   ████     ██      ████              ████      ████            ███                ███            ████       ████        ███                 ███            ████  \n"                 
-                            "    ███    ███      ███               ████      ████            ██                 ███             ████      ████        ███       █         ███             ████ \n"                  
-                            "     ███   ██      ████                ████     ████            ██                 ███             ████      ████        ███      ██         ███             ████ \n"                  
-                            "     ████ ██       ████                ████     ████            ██                 ███              ███      ████        ███████████         ███              ████\n"                  
-                            "      █████        ████                ████     ████            ██                 ███             ████      ████        ███████████         ███              ████\n"                  
-                            "       ███         ████                ████     ████            ██                 ███             ████      ████        ███      ██         ███             ████ \n"                 
-                            "       ███          ████               ███       ███            ██                 ███             ████      ████        ███                 ███             ████ \n"                  
-                            "       ███          █████             ████       ███            ██                 ███            ████       ████        ███           █     ███            ████  \n"                  
-                            "       ███           █████           ████        ████          ██                  ███           ████        ████        ███          ██     ███           ████   \n"                  
-                            "       ████            █████       ████           █████      ████                  ████       ██████         ████        ████        ███     ████        █████    \n"                  
-                            "    ██████████           ████████████              ████████████  █              █████████████████         █████████   █████████████████   █████████████████       \n"                 
-                            "    ██      █               ██████                     █████                     █      █████              █      █    ██                  █     ██████           \n";                   
-                                                                                                                                                                                                        
-                                                                                                                                                                                                        
-                cout << "cai jiu duo lian\n";
-                break;
-            }
-            cout<<"enter the mode and the coordinates! modes: 'e': excavate, 'f': flag, 'q': quit (e.g.: e x-axis y-axis)\n";
-        }
-        cout << "end testing"<<endl;
-    }
-    else if (options == "L") {
-        int ** initial_board;
-        int x,y;
-        initial_board = read_file("Record.txt",x,y);
-        x+=1;
-        y+=1;
-        display(initial_board, x, y, 0, 0);
-
-        //test input
+void in_main(int ** initial_board,int x,int y) {
+    //test input
         string temp;
         char mode;
         int input_x=999, input_y=999;
@@ -395,6 +285,60 @@ int main() {
             cout<<"enter the mode and the coordinates! modes: 'e': excavate, 'f': flag, 'q': quit (e.g.: e x-axis y-axis)\n";
         }
         cout << "end testing"<<endl;
+}
+
+int main() {
+// customer input (separate function??) //
+    string options;
+    string xyl;
+    int x=1, y=1, level=0;  
+    cout << "what you want to do?\n New Game: N\n Load Game: L\n";
+    cin >> options;  
+    if (options == "N") {
+        while( y<3 || y>300 ){
+            cout << "The width of the board (3-300): ";
+            cin>>xyl;
+            istringstream iss(xyl);
+            iss >> y;
+        }
+        while( x<3 || x>300 ){
+            cout << "The height of the board (3-300): ";
+            cin>>xyl;
+            istringstream iss(xyl);
+            iss >> x;
+        }
+        while ( level<1 || level>10 ){
+            cout << "Choose a level (1-10):";
+            cin>>xyl;
+            istringstream iss(xyl);
+            iss >> level;
+        }
+        int num_of_mine = 0.1*level*x*y/5; //set 地雷 value
+        if (num_of_mine<1){ //set minimum 地雷 value
+            num_of_mine=1;
+        }
+    // //
+    // initialise_board //
+        int **initial_board = new int*[x];
+        for ( int i=0; i<x; ++i){
+            initial_board[i] = new int[y];
+        }
+
+        initialise_board(initial_board, x, y, num_of_mine);
+    //  //
+        display(initial_board, x, y, 0, 0);
+
+        //test input
+        in_main(initial_board,x,y);
+    }
+    else if (options == "L") {
+        int ** initial_board;
+        int x,y;
+        initial_board = read_file("Record.txt",x,y);
+        x+=1;
+        y+=1;
+        display(initial_board, x, y, 0, 0);
+        in_main(initial_board,x,y);
     }
     return 0;
 }
