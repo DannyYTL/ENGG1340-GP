08f881b5f617718760186ce642eca48b6e1b4295
Wind.D <125264206+DannyYTL@users.noreply.github.com>
2024-04-20 23:51:32 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -291,7 +291,11 @@ int main() {
     string xyl;
     int x=1, y=1, level=0;  
     cout << "what you want to do?\n New Game: N\n Load Game: L\n";
-    cin >> options;  
+    cin >> options;
+    while (options != "N" && options != "L") {
+        cout << "Invalid option, please choose again ( N - new game; L - continue from last game ): " << endl;
+        cin >> options;
+    }    
     if (options == "N") {
         while( y<3 || y>300 ){
             cout << "The width of the board (3-300): ";
