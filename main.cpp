ea56a7ff93e9e70ee8e356a860253f457bc8819d
Wind.D <125264206+DannyYTL@users.noreply.github.com>
2024-04-20 13:53:58 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -192,10 +192,12 @@ void save_to_file(string filename,int ** board,int x,int y) {
     return;
 }
 
-int ** read_file(string filename) {
+int ** read_file(string filename,int &x,int &y) {
         ifstream fin;
-        string thisline;
-        int thisnum,x=-1,y=-1,count=0;
+        string thisline,thisnum;
+        x=-1;
+        y=-1;
+        int count=0;
         int ** board = new int*[300];
         for (int i=0; i<300; ++i){
             board[i] = new int[300];
@@ -206,15 +208,14 @@ int ** read_file(string filename) {
         }
         else {
             while (getline(fin,thisline)) {
-                cout << count << "  " << thisline<< endl;
-                count += 1;
+                //cout << count << "  " << thisline<< endl;
+                //count += 1;
                 istringstream iss(thisline);
                 x += 1;
                 y = -1;
                 while (iss >> thisnum) {
-                    cout << thisnum << endl
                     y += 1;
-                    board[x][y] = thisnum;
+                    board[x][y] = stoi(thisnum);
                 }
             }
             fin.close();
@@ -290,7 +291,7 @@ int main() {
                 continue;
             }
             if(mode=='q'){
-                save_to_file("Record.txt",initial_board);
+                save_to_file("Record.txt",initial_board,x,y);
                 cout << "Bye\n";
                 break;
             }
@@ -330,8 +331,74 @@ int main() {
         cout << "end testing"<<endl;
     }
     else if (options == "L") {
-        initial_board = read_file("Record.txt");
-        
+        int ** initial_board;
+        int x,y;
+        initial_board = read_file("Record.txt",x,y);
+        display(initial_board, x, y, 0, 0);
+
+        //test input
+        string temp;
+        char mode;
+        int input_x=999, input_y=999;
+        bool die = 0, valid=0;
+        //cout<<"max_x: "<<y<<", max_y: "<<x<<endl;
+    
+        cout<<"enter the mode and the coordinates!" << endl;
+        cout << "modes: 'e': excavate, 'f': flag, 'q': quit (e.g.: e x-axis y-axis)\n";
+        while(getline(cin,temp)){
+            istringstream iss(temp);
+            iss >> mode >> input_x >> input_y;
+            if (mode != 'q' && mode != 'e' && mode != 'f'){
+                cout<<"Please enter a valid mode"<<endl;
+                input_x=999;
+                input_y=999;
+                continue;
+            }
+            else if (mode != 'q' && (input_x < 0 || input_x >= y || input_y < 0 || input_y >= x)){
+                cout<<"Please enter a valid coordinates"<<endl;
+                input_x=999;
+                input_y=999;
+                continue;
+            }
+            if(mode=='q'){
+                save_to_file("Record.txt",initial_board,x,y);
+                cout << "Bye\n";
+                break;
+            }
+            
+            die = processing(initial_board, y, x, mode, input_x, input_y);
+            if(die !=1 && win(initial_board,x,y)==1){
+                display(initial_board, x, y, die, win);
+                cout << "you win\n";
+                break;
+            }
+            display(initial_board, x, y, die, 0);
+            if(die){                                                                                                                                                                                       
+                   cout <<  "█     █       █    █         ██████            █      █        █     █                                      █      █    █     █  █                   █            \n"        
+                            "████████     ███████      █████████████        █████████      ████████            ████████████████         █████████    ███████████████     ████████████████      \n"                      
+                            " ████         ███      ████        █████        ████            ███               ████       ███████         ████        ███        ██       ████      ███████    \n"                     
+                            "  ████       ██      ████            ████       ████            ███                ███           ████      █ ████        ███         █       ███           ████   \n"                  
+                            "   ████     ██      ████              ████      ████            ███                ███            ████       ████        ███                 ███            ████  \n"                 
+                            "    ███    ███      ███               ████      ████            ██                 ███             ████      ████        ███       █         ███             ████ \n"                  
+                            "     ███   ██      ████                ████     ████            ██                 ███             ████      ████        ███      ██         ███             ████ \n"                  
+                            "     ████ ██       ████                ████     ████            ██                 ███              ███      ████        ███████████         ███              ████\n"                  
+                            "      █████        ████                ████     ████            ██                 ███             ████      ████        ███████████         ███              ████\n"                  
+                            "       ███         ████                ████     ████            ██                 ███             ████      ████        ███      ██         ███             ████ \n"                 
+                            "       ███          ████               ███       ███            ██                 ███             ████      ████        ███                 ███             ████ \n"                  
+                            "       ███          █████             ████       ███            ██                 ███            ████       ████        ███           █     ███            ████  \n"                  
+                            "       ███           █████           ████        ████          ██                  ███           ████        ████        ███          ██     ███           ████   \n"                  
+                            "       ████            █████       ████           █████      ████                  ████       ██████         ████        ████        ███     ████        █████    \n"                  
+                            "    ██████████           ████████████              ████████████  █              █████████████████         █████████   █████████████████   █████████████████       \n"                 
+                            "    ██      █               ██████                     █████                     █      █████              █      █    ██                  █     ██████           \n";                   
+                                                                                                                                                                                                        
+                                                                                                                                                                                                        
+                    
+                cout << "cai jiu duo lian\n";
+                break;
+            }
+            cout<<"enter the mode and the coordinates! modes: 'e': excavate, 'f': flag, 'q': quit (e.g.: e x-axis y-axis)\n";
+        }
+        cout << "end testing"<<endl;
     }
     return 0;
 }
