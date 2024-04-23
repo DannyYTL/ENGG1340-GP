28d8d282f5d1eaa11b964aa08ab56c755a1a91f7
chenyrn <103996844+chenyrn@users.noreply.github.com>
2024-04-23 17:26:50 +0800

    Update win.cpp

--- win.cpp
+++ win.cpp
@@ -3,7 +3,7 @@
 //input parameters are: pointer to a 2D-array, int x and y representing the size of board.
 //return boolean type true or false.
 bool win(int **board, int x, int y) {
-    for (int i=0; i<x; ++i){
+    for (int i=0; i<x; ++i){    // loop to search in board.
         for (int j=0; j<y; ++j){
             if (board[i][j]==9)
                 return 0; // not won if there is remaining mine.
