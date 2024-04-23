2ccaf9a67586fdc0e9559106686958e716edfe67
chenyrn <103996844+chenyrn@users.noreply.github.com>
2024-04-23 17:36:02 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -262,7 +262,6 @@ void in_main(int ** initial_board,int x,int y) {
 }*/// 改动 in_main.cpp 
 
 int main() {
-// customer input (separate function??) //
     string options;
     string xyl;
     int x=1, y=1, level=0;  
@@ -275,32 +274,32 @@ int main() {
     if (options == "N") {
         while( y<3 || y>300 ){
             cout << "The width of the board (3-300): ";
-            cin>>xyl;
+            cin>>xyl;        //get user input of board width
             istringstream iss(xyl);
             iss >> y;
         }
         while( x<3 || x>300 ){
             cout << "The height of the board (3-300): ";
-            cin>>xyl;
+            cin>>xyl;        //get user input of board height
             istringstream iss(xyl);
             iss >> x;
         }
         while ( level<1 || level>10 ){
             cout << "Choose a level (1-10):";
-            cin>>xyl;
+            cin>>xyl;        //get user input of the difficulty of game
             istringstream iss(xyl);
             iss >> level;
         }
-        int num_of_mine = 0.1*level*x*y/5;         //set 地雷 value
-        if (num_of_mine<1){                        //set minimum 地雷 value
+        int num_of_mine = 0.1*level*x*y/5;         //set the number of mine according to the input level and size of board
+        if (num_of_mine<1){                        //minimum value of mine should be 1
             num_of_mine=1;
         }
     // initialise_board //
-        int **initial_board = new int*[x];
+        int **initial_board = new int*[x];        // create a dynamic 2D array to represent an empty board
         for ( int i=0; i<x; ++i){
             initial_board[i] = new int[y];
         }
-        initialise_board(initial_board, x, y, num_of_mine);
+        initialise_board(initial_board, x, y, num_of_mine); // initialise the board
     //                  //
         display(initial_board, x, y, 0, 0);
         in_main(initial_board,x,y);
