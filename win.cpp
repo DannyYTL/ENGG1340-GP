9c34c5309c69d100ed610b6be00418476adb7f52
chenyrn <103996844+chenyrn@users.noreply.github.com>
2024-04-23 16:56:54 +0800

    Update win.cpp

--- win.cpp
+++ win.cpp
@@ -1,12 +1,12 @@
 #include "gaming.h"
-
+//Determining whether the game is won or not.
 
 bool win(int **board, int x, int y) {
     for (int i=0; i<x; ++i){
         for (int j=0; j<y; ++j){
             if (board[i][j]==9)
-                return 0;
+                return 0; // not won if there is remaining mine.
         }
     }
-    return 1;
+    return 1; // otherwise won.
 }
